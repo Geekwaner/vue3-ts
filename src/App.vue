4cd8c430a2478c51d8103220e3077958b2ab29b5
有一只母猪 <297542455@qq.com>
2022-12-15 17:56:34 +0800

    16-Vite 配置-自动导入样式

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,6 @@ getList();
 </template>
 
 <style lang="less" scoped>
-@import '@/assets/styles/variables.less';
-@import '@/assets/styles/mixins.less';
 h1 {
   background-color: @sucColor;
   .hoverShadow ();
