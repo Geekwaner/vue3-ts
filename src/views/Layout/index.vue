0088f4b0d6510cb6dd7cc7dde2e7c9631407d618
Seven <297542455@qq.com>
2022-12-17 14:51:14 +0800

    11-吸顶静态

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,6 +2,7 @@
 import AppTopnav from './components/app-topnav.vue';
 import AppHeader from './components/app-header.vue';
 import AppFooter from './components/app-footer.vue';
+import AppHeaderSticky from './components/app-header-sticky.vue';
 </script>
 
 <template>
@@ -9,6 +10,8 @@ import AppFooter from './components/app-footer.vue';
   <AppTopnav />
   <!-- 模块2：头部组件 -->
   <AppHeader />
+  <!-- 模块2：顶部吸顶 -->
+  <AppHeaderSticky></AppHeaderSticky>
   <!-- 模块3：页面底部 -->
   <AppFooter />
 </template>
