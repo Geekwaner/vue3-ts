6fda126d532d1ad382149692fe93e04762c0402b
Seven <297542455@qq.com>
2022-12-18 14:59:15 +0800

    09-检测元素可见性封装到函数

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -8,7 +8,6 @@ import { ref } from 'vue';
 2. 把变量 通过 ref 属性和 dom 关联起来
 3. 页面渲染完之后，就可以使用了
 */
-const target = ref(null);
 
 // 🔔核心单词解释：
 //   useIntersectionObserver   检查元素是否进入可视区函数
@@ -17,15 +16,31 @@ const target = ref(null);
 //   stop                      用于停止检测的函数
 
 // 官网基本案例，供同学练习复制
-const { stop } = useIntersectionObserver(target, ([{ isIntersecting }]) => {
-  // 写业务代码
-  console.log('isIntersecting目标是否进入可视区 -----> ', isIntersecting);
-  if (isIntersecting) {
-    console.log('目标进入了可视区，停止监听 ');
-    // 实现业务代码
-    stop();
-  }
-});
+const useObserver = () => {
+  const target = ref(null);
+  const { stop } = useIntersectionObserver(target, ([{ isIntersecting }]) => {
+    // 写业务代码
+    console.log('isIntersecting目标是否进入可视区 -----> ', isIntersecting);
+    if (isIntersecting) {
+      console.log('目标进入了可视区，停止监听 ');
+      // 实现业务代码
+      stop();
+    }
+  });
+
+  return { target };
+};
+const { target } = useObserver();
+console.log('target -----> ', target);
+// const fn = () => {
+//   alert(123);
+//   console.log('456 -----> ', 456);
+//   const a = 1;
+//   const b = 1;
+//   return { a, b };
+// };
+// const { a } = fn();
+// console.log('a -----> ', a);
 </script>
 
 <template>
