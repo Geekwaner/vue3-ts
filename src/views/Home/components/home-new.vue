e127d82d46b915bd569f0d6caa16bb00f8b97f55
Seven <297542455@qq.com>
2022-12-18 12:27:38 +0800

    08-具体业务实现

--- src/views/Home/components/home-new.vue
+++ src/views/Home/components/home-new.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 import { useHomeStore } from '@/store';
+import { useIntersectionObserver } from '@vueuse/core';
+import { ref } from 'vue';
 import HomePanel from './home-panel.vue';
 const home = useHomeStore();
-home.getNewGoodsList();
+// home.getNewGoodsList();
+
+const target = ref(null);
+const { stop } = useIntersectionObserver(target, ([{ isIntersecting }]) => {
+  // 写业务代码
+  console.log('isIntersecting目标是否进入可视区 -----> ', isIntersecting);
+  if (isIntersecting) {
+    console.log('目标进入了可视区，停止监听 ');
+    // 实现业务代码
+    home.getNewGoodsList();
+    stop();
+  }
+});
 </script>
 
 <template>
+  <div style="height: 2000px"></div>
   <div class="home-new">
-    <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+    <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱" ref="target">
       <template #right><XtxMore path="/" /></template>
       <!-- 面板内容 -->
       <ul class="goods-list">
