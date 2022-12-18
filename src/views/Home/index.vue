b29c73d1756a154afa7f13ac1468df7480ee4f94
Seven <297542455@qq.com>
2022-12-18 10:54:17 +0800

    05-面板组件的封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HomeBanner from './components/home-banner.vue';
 import HomeCategory from './components/home-category.vue';
+import HomePanel from './components/home-panel.vue';
 </script>
 
 <template>
@@ -13,6 +14,12 @@ import HomeCategory from './components/home-category.vue';
         <HomeBanner />
       </div>
     </div>
+    <HomePanel title="我是主标题" subTitle="我是副标题">
+      <template #right> 我是右侧插槽 </template>
+      <h1>我是默认插槽</h1>
+      <div>多写几个也没问题</div>
+      <p>默认插槽段落</p>
+    </HomePanel>
   </div>
 </template>
 
