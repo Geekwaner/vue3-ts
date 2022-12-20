e919927adae9f2c635f668f3b2a0d0e130d80d95
有一只母猪 <297542455@qq.com>
2022-12-20 10:43:41 +0800

    04-商品详情静态

--- src/views/Goods/index.vue
+++ src/views/Goods/index.vue
@@ -29,10 +29,73 @@ onMounted(async () => {
     <div class="container">
       <!-- 商品信息 -->
       <div class="goods-info">
-        <!-- 图片预览区 -->
-        <div class="media"></div>
+        <div class="media">
+          <!-- 图片预览区 -->
+          <div class="goods-image">
+            <!-- 图片预览组件 -->
+          </div>
+          <!-- 统计数量,后台没返回数据,不做渲染 -->
+          <ul class="goods-sales">
+            <li>
+              <p>销量人气</p>
+              <p>100+</p>
+              <p><i class="iconfont icon-task-filling"></i>销量人气</p>
+            </li>
+            <li>
+              <p>商品评价</p>
+              <p>200+</p>
+              <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
+            </li>
+            <li>
+              <p>收藏人气</p>
+              <p>80+</p>
+              <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
+            </li>
+            <li>
+              <p>品牌信息</p>
+              <p>90+</p>
+              <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
+            </li>
+          </ul>
+        </div>
         <!-- 商品信息区 -->
-        <div class="spec"></div>
+        <div class="spec">
+          <!-- 商品主要信息 -->
+          <div class="goods-main">
+            <p class="g-name">这是商品标题</p>
+            <p class="g-desc">这是商品描述</p>
+            <p class="g-desc">这是选中的商品规格</p>
+            <p class="g-price">
+              <span>商品现在的价钱</span>
+              <span>商品原来的价格</span>
+            </p>
+            <div class="g-service">
+              <dl>
+                <dt>促销</dt>
+                <dd>12月好物放送，App领券购买直降120元</dd>
+              </dl>
+              <dl>
+                <dt>配送</dt>
+                <dd>至</dd>
+                <dd>
+                  <XtxCity />
+                </dd>
+              </dl>
+              <dl>
+                <dt>服务</dt>
+                <dd>
+                  <span>无忧退货</span>
+                  <span>快速退款</span>
+                  <span>免费包邮</span>
+                  <a href="javascript:;">了解详情</a>
+                </dd>
+              </dl>
+            </div>
+          </div>
+          <!-- 规格选择组件 -->
+          <!-- 数量选择组件 -->
+          <!-- 按钮组件 -->
+        </div>
       </div>
       <!-- 商品详情 -->
       <div class="goods-footer">
@@ -51,6 +114,8 @@ onMounted(async () => {
 .container {
   margin-top: 20px;
 }
+
+// 商品信息
 .goods-info {
   min-height: 600px;
   background: #fff;
@@ -65,6 +130,118 @@ onMounted(async () => {
     padding: 30px 30px 30px 0;
   }
 }
+
+// 图片预览区
+.goods-image {
+  width: 480px;
+  height: 400px;
+  background-color: #eee;
+}
+
+// 统计数量
+.goods-sales {
+  display: flex;
+  width: 400px;
+  align-items: center;
+  text-align: center;
+  height: 140px;
+  li {
+    flex: 1;
+    position: relative;
+    ~ li::after {
+      position: absolute;
+      top: 10px;
+      left: 0;
+      height: 60px;
+      border-left: 1px solid #e4e4e4;
+      content: '';
+    }
+    p {
+      &:first-child {
+        color: #999;
+      }
+      &:nth-child(2) {
+        color: @priceColor;
+        margin-top: 10px;
+      }
+      &:last-child {
+        color: #666;
+        margin-top: 10px;
+        i {
+          color: @xtxColor;
+          font-size: 14px;
+          margin-right: 2px;
+        }
+        &:hover {
+          color: @xtxColor;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+
+// 商品信息区
+.spec {
+  .g-name {
+    font-size: 22px;
+  }
+  .g-desc {
+    color: #999;
+    margin-top: 10px;
+  }
+  .g-price {
+    margin-top: 10px;
+    span {
+      &::before {
+        content: '¥';
+        font-size: 14px;
+      }
+      &:first-child {
+        color: @priceColor;
+        margin-right: 10px;
+        font-size: 22px;
+      }
+      &:last-child {
+        color: #999;
+        text-decoration: line-through;
+        font-size: 16px;
+      }
+    }
+  }
+  .g-service {
+    background: #f5f5f5;
+    width: 500px;
+    padding: 20px 10px 0 10px;
+    margin-top: 10px;
+    dl {
+      padding-bottom: 20px;
+      display: flex;
+      align-items: center;
+      dt {
+        width: 50px;
+        color: #999;
+      }
+      dd {
+        color: #666;
+        &:last-child {
+          span {
+            margin-right: 10px;
+            &::before {
+              content: '•';
+              color: @xtxColor;
+              margin-right: 2px;
+            }
+          }
+          a {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
+
 .goods-footer {
   display: flex;
   margin-top: 20px;
