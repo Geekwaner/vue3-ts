405d0d8763abdda33b20c780f4fccd248e7bf9a9
有一只母猪 <297542455@qq.com>
2022-12-21 16:48:46 +0800

    12-QQ登录的环境配置-记得修改电脑hosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,25 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // QQ三方登录的回调uri为：http://www.corho.com:8080/#/login/callback
+  // vite 中配置： www.corho.com:8080
+  // 记得还要修改电脑 hosts 文件
+  // 1. 找到 C:\Windows\System32\drivers\etc 下 hosts 文件
+  // 2. 在文件中加入  127.0.0.1    www.corho.com
+  // 3. 保存即可
+  server: {
+    // 主机名
+    host: 'www.corho.com',
+    // 端口
+    port: 8080,
+    // 其他有价值选项
+    // 允许跨域，默认其实就是true
+    cors: true,
+    // 服务代理
+    proxy: {},
+    // 启动服务，自动打开浏览器
+    open: true,
+  },
   plugins: [
     // 需要主动开启响应式语法糖的属性
     vue({
