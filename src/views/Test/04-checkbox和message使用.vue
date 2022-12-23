9a0f33b4fe9c49137e31eefe7d2bfc0dc31510c4
有一只母猪 <297542455@qq.com>
2022-12-23 11:44:15 +0800

    07-倒计时静态

---
+++ src/views/Test/04-checkbox和message使用.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { message } from '@/components/XtxUI';
+import { ref } from 'vue';
+
+const isAgree = ref(false);
+
+const showMsg = () => {
+  message({ text: '当前选中' + isAgree.value, type: 'success', time: 2000 });
+};
+</script>
+
+<template>
+  <h1>Hello vue3 + ts👍</h1>
+  <XtxCheckBox v-model="isAgree" @click="showMsg">我已同意霸王条款</XtxCheckBox>
+</template>
+
+<style lang="less" scoped></style>
