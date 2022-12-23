be68366efebb9f3a5c70a203e491f880ba403d26
有一只母猪 <297542455@qq.com>
2022-12-23 18:01:30 +0800

    17-使用reduce实现加总

--- src/store/modules/cart.ts
+++ src/store/modules/cart.ts
@@ -18,18 +18,30 @@ export const useCartStore = defineStore('cart', {
     },
 
     // 计算有效商品的数量
-    effectiveListCount() {
+    effectiveListCount(): number {
       // 写法1，保证业务实现
-      let sum = 0;
-      this.effectiveList.forEach((v) => (sum += v.count));
-      return sum;
+      // let sum = 0;
+      // this.effectiveList.forEach((v) => (sum += v.count));
+      // return sum;
+
+      /*
+      reduce 对数组遍历，一般用来求和，
+      简单来说，传入 总数 和 每一项，把每一项的值和总数相加，再返回，加到最后就是总和
+
+      reduce( fn(初始值/上一次调用返回的数，item当前遍历的对象), 初始值)
+      */
+      return this.effectiveList.reduce((sum, item) => sum + item.count, 0);
     },
     // 有效商品总价格
-    effectiveListPrice() {
+    effectiveListPrice(): number {
       // 写法1，保证业务实现
-      let sum = 0;
-      this.effectiveList.forEach((v) => (sum += Number(v.nowPrice) * v.count));
-      return sum;
+      // let sum = 0;
+      // this.effectiveList.forEach((v) => (sum += Number(v.nowPrice) * v.count));
+      // return sum;
+      return this.effectiveList.reduce(
+        (sum, item) => sum + Number(item.nowPrice) * item.count,
+        0
+      );
     },
   },
   // 方法
