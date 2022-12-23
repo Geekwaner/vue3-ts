726b8366b2f2227120cc124ced991e399665094b
有一只母猪 <297542455@qq.com>
2022-12-23 15:44:53 +0800

    12-购物车模块

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 // 统一导入导出
 export * from './modules/home';
 export * from './modules/member';
+export * from './modules/cart';
 
 // 最终导出 { useHomeStore , useMemberStore }
 // 页面要使用 import { useHomeStore , useMemberStore } from './路径'
---
+++ src/store/modules/cart.ts
@@ -0,0 +1,19 @@
+import { http } from '@/utils/request';
+import { defineStore } from 'pinia';
+
+export const useCartStore = defineStore('cart', {
+  // 状态
+  state: () => ({
+    // 购物车列表
+    list: [],
+  }),
+  // 计算
+  getters: {},
+  // 方法
+  actions: {
+    // 加入购物车
+    async addCart(data: object) {
+      const res = await http('post', '/member/cart', data);
+    },
+  },
+});
