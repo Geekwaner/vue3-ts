ec0e57c2820d842b554f4b6a6a363db66423a0a1
有一只母猪 <297542455@qq.com>
2022-12-26 16:12:21 +0800

    13-Suspense解开promise

---
+++ src/views/Test/AsyncCom.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { http } from '@/utils/request';
+// 如果在顶层发送了 await 请求，请保证祖先元素内有使用 Suspense
+const res = await http('get', '/home/banner');
+console.log('res -----> ', res);
+</script>
+
+<template>
+  <h1>异步加载组件...👍</h1>
+</template>
+
+<style lang="less" scoped></style>
--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -3,15 +3,22 @@
 // import MyButton from './MyButton.vue';
 
 import { defineAsyncComponent } from 'vue';
+import AsyncCom from './AsyncCom.vue';
 // 异步加载组件，可以用到vue3的defineAsyncComponent
 const TestCom = defineAsyncComponent(() => import('./MyButton.vue'));
 </script>
 
 <template>
   <h1>Hello vue3 + ts👍</h1>
+  <!-- 不在 Suspense里面，没人去解开组件内的promise -->
+  <!-- <AsyncCom></AsyncCom> -->
   <!-- Suspense可以给异步组件添加等待状态 -->
   <Suspense>
-    <TestCom>异步加载的按钮</TestCom>
+    <!-- 可以解开组件中的 promise  -->
+    <div>
+      <TestCom>异步加载的按钮</TestCom>
+      <AsyncCom></AsyncCom>
+    </div>
 
     <template #fallback>
       <h1>
