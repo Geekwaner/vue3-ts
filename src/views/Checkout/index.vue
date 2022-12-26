94152485176436c7c22e47fd70935de6fbebd931
有一只母猪 <297542455@qq.com>
2022-12-26 12:08:54 +0800

    07-封装到utils

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -2,6 +2,7 @@
 import { useCheckoutStore } from '@/store';
 import { storeToRefs } from 'pinia';
 import { computed, ref } from 'vue';
+import { hideContact } from '@/utils';
 const checkout = useCheckoutStore();
 checkout.getCheckouInfo();
 
@@ -25,24 +26,6 @@ const index = ref(0);
 const currentAddress = computed(() => {
   return checkout.checkoutInfo.userAddresses?.[index.value] || {};
 });
-
-// 隐藏手机号中间4位
-const hideContact = (contact: string) => {
-  // 写法1，slice实现
-  // return contact.slice(0, 3) + '****' + contact.slice(-4);
-
-  // 写法2，replace配合正则
-  /*
-    正则分组
-    一般配合 replace 使用
-    N个小括号用来分组，
-    $1占位，代表第1个小括号的内容
-    $2占位，代表第2个小括号的内容
-    ...
-    $N占位，代表第N个小括号的内容
-  */
-  return contact.replace(/(\d{3})(\d{4})(\d{4})/, '$1****$3');
-};
 </script>
 
 <template>
