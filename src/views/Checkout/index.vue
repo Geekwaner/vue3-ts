e4b2d3b5d8a179fdb34d0ccedfce436dd9e161e6
有一只母猪 <297542455@qq.com>
2022-12-26 11:26:25 +0800

    06-正则隐藏手机号码中间4位

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -29,7 +29,19 @@ const currentAddress = computed(() => {
 // 隐藏手机号中间4位
 const hideContact = (contact: string) => {
   // 写法1，slice实现
-  return contact.slice(0, 3) + '****' + contact.slice(-4);
+  // return contact.slice(0, 3) + '****' + contact.slice(-4);
+
+  // 写法2，replace配合正则
+  /*
+    正则分组
+    一般配合 replace 使用
+    N个小括号用来分组，
+    $1占位，代表第1个小括号的内容
+    $2占位，代表第2个小括号的内容
+    ...
+    $N占位，代表第N个小括号的内容
+  */
+  return contact.replace(/(\d{3})(\d{4})(\d{4})/, '$1****$3');
 };
 </script>
 
