450da8016bf5c2357d0541b2b390eb9d3d4d4f52
有一只母猪 <297542455@qq.com>
2022-12-27 17:41:57 +0800

    18-jsx的体验

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -1,21 +1,41 @@
-<script setup lang="ts">
-import { onMounted, createVNode } from 'vue';
+<script setup lang="tsx">
+// import { onMounted, createVNode, h } from 'vue';
 
-onMounted(() => {
-  const h1DOM = document.querySelector('h1');
-  console.dir(h1DOM);
-});
+// onMounted(() => {
+//   const h1DOM = document.querySelector('h1');
+//   console.dir(h1DOM);
+// });
 
-const DIVDOM = createVNode('div', {}, '我是div标签');
-const VDOM = createVNode('h1', { id: 'box', title: '虚拟title' }, [DIVDOM]);
+// const DIVDOM = h('div', {}, '我是div标签');
+// const VDOM = h('h1', { id: 'box', title: '虚拟title' }, [
+//   h('div', {}, '我是div标签'),
+//   h('div', {}, '我是div标签'),
+//   DIVDOM,
+// ]);
 
-const UL = createVNode('ul', {}, [
-  createVNode('li', {}, '111'),
-  createVNode('li', {}, '222'),
-  createVNode('li', {}, '333'),
-  createVNode('li', {}, '444'),
-]);
-console.dir(VDOM);
+// const UL = h('ul', {}, [
+//   h('li', {}, '111'),
+//   h('li', {}, '222'),
+//   h('li', {}, '333'),
+//   h('li', {}, '444'),
+// ]);
+// console.dir(VDOM);
+
+// jsx/tsx 允许我们在 js 中写 html 文档结构
+const VDOM = () => (
+  <h1 id="box" title="虚拟title">
+    我是虚拟h1
+  </h1>
+);
+
+const UL = () => (
+  <ul>
+    <li>111</li>
+    <li>222</li>
+    <li>333</li>
+    <li>444</li>
+  </ul>
+);
 </script>
 
 <template>
